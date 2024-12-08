b7bd1b68d0ed7af427070bc85d9b0d9e58e304c4
Princess-2017 <mohammadpoor.tahere@gmail.com>
2024-12-09 00:59:53 +0330

    complete appointments

--- Microservices/TradeBuddy.Appointment/TradeBuddy.Appointment.Domain/Entities/Appointment.cs
+++ Microservices/TradeBuddy.Appointment/TradeBuddy.Appointment.Domain/Entities/Appointment.cs
@@ -4,10 +4,43 @@ namespace TradeBuddy.Appointment.Domain.Entities
 {
     public class Appointment : BaseEntity<AppointmentId>
     {
-        public virtual BusinessId BusinessId { get; private set; } // شناسه کسب و کار
-        public virtual CustomerId CustomerId { get; private set; } // شناسه مشتری
-        public virtual Time Time { get; private set; } // زمان نوبت
-        public DateTime AppointmentDate { get; private set; } // تاریخ نوبت
-        public AppointmentStatus Status { get; private set; } // وضعیت نوبت
+        public virtual BusinessId BusinessId { get; private set; }
+        public virtual CustomerId CustomerId { get; private set; }
+        public virtual Time Time { get; private set; }
+        public DateTime AppointmentDate { get; private set; }
+        public AppointmentStatus Status { get; private set; }
+
+        // Constructor for creating a new Appointment
+        public Appointment(
+            BusinessId businessId,
+            CustomerId customerId,
+            Time time,
+            DateTime appointmentDate,
+            AppointmentStatus status)
+        {
+            BusinessId = businessId;
+            CustomerId = customerId;
+            Time = time;
+            AppointmentDate = appointmentDate;
+            Status = status;
+        }
+
+        // Parameterless constructor for EF Core
+        private Appointment() { }
+
+        // Method for updating an appointment
+        public void Update(
+            BusinessId businessId,
+            CustomerId customerId,
+            Time time,
+            DateTime appointmentDate,
+            AppointmentStatus status)
+        {
+            BusinessId = businessId;
+            CustomerId = customerId;
+            Time = time;
+            AppointmentDate = appointmentDate;
+            Status = status;
+        }
     }
 }
